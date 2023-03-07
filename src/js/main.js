631e1e39d639ce97b24ff356af9701e09497f91a
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-03-07 12:09:32 +0100

    Footer fix, nav fix
    Footer : added hover on links, navigation : scroll fix on Iphone safari, navigation links : paths fix, open hamburger after dekstop breakpoint fix : js resize event added

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,13 @@ const mobileLinks = document.querySelectorAll('.nav__mobile-link')
 const footerYear = document.querySelector('.year')
 const nav = document.querySelector('.nav')
 let lastScrollY = window.scrollY
+let lastWidthX = window.innerWidth
 
 const hamburgerActive = () => {
 	navBtn.classList.toggle('is-active')
 	menu.classList.toggle('nav__mobile--active')
 	body.classList.toggle('no-scroll-mobile')
-	mobileLinks.forEach((link) => {
+	mobileLinks.forEach(link => {
 		link.addEventListener('click', () => {
 			navBtn.classList.remove('is-active')
 			menu.classList.remove('nav__mobile--active')
@@ -19,6 +20,7 @@ const hamburgerActive = () => {
 	})
 }
 
+//get currentyear in the footer
 const currentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
@@ -27,6 +29,7 @@ const currentYear = () => {
 navBtn.addEventListener('click', hamburgerActive)
 currentYear()
 
+//collapse expand menu depending on scroll
 window.addEventListener('scroll', () => {
 	if (lastScrollY < window.scrollY) {
 		nav.classList.add('nav--hidden')
@@ -34,6 +37,22 @@ window.addEventListener('scroll', () => {
 		nav.classList.remove('nav--hidden')
 	}
 
+	//for iphone safari issue
+	if (lastScrollY <= 0 && window.scrollY == 0) {
+		nav.classList.remove('nav--hidden')
+	}
+
 	lastScrollY = window.scrollY
 })
 
+//remove open mobile nav after dekstop breakpoint
+window.addEventListener('resize', () => {
+	lastWidthX = window.innerWidth
+
+	if (lastWidthX >= 992) {
+		console.log(`${lastWidthX} Większe niż 992`)
+		navBtn.classList.remove('is-active')
+		menu.classList.remove('nav__mobile--active')
+		body.classList.remove('no-scroll-mobile')
+	}
+})
